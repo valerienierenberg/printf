42c9c8e8511aaad62483b97f4aad1e2614f5d93e
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-25 21:19:27 +0000

    Remove extra printf.c recovery file

--- _printf.c
+++ _printf.c
@@ -7,27 +7,6 @@
 
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-nt print (char * str, ...)
-{
-va_list args;
-int i = 0
-int j = 0;
-char * str_arg;
-
-	va_start(args, str);
-	while (str && str[i])
-	{
-	if(str[i] == '%')
-	{
-	i++;
-	if (str[i] == 'c')
-	{
-
-
-
-=======
->>>>>>> 6c8f70de6d489a4c1c7829a973958de131ce67bc
 
 va_list args;
 int i = 0;
@@ -40,5 +19,4 @@ _printf ops[] = {
 	{'i', op_int},
 	{'\0', NULL}
 };
-
 
