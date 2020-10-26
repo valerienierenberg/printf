5a1eacf48eec3f17321aae5f1724e83a7303c63a
laurendobratz00 <laurendobratz@icloud.com>
2020-10-26 15:28:39 +0000

    Changes made to holberton.h

--- holberton.h
+++ holberton.h
@@ -6,11 +6,11 @@ int _putchar(char c);
 
 typedef struct printf_
 {
-	char *op;
+	char op;
 	int (*f)(va_list);
 } printf_;
 
-
+void _puts(char *str);
 int op_char(va_list one);
 int op_string(va_list two);
 int op_mod(va_list three);
