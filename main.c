63d113c654ab09de7485eb94494b5fb7ef3c35fb
laurendobratz00 <laurendobratz@icloud.com>
2020-10-26 21:06:43 +0000

    Pushes everything

--- main.c
+++ main.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    return (0);
+ _printf("Character:[%c]\n", 'H');
+  printf("Character:[%c]\n", 'H');
+ _printf("String:[%s]\n", "I am a string !");
+  printf("String:[%s]\n", "I am a string !");
+  return (0);
 }
 
