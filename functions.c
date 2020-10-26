672d7d6af2e5ee65961771e58aec516e5dbee13a
laurendobratz00 <laurendobratz@icloud.com>
2020-10-26 19:44:22 +0000

    Changes made

--- functions.c
+++ functions.c
@@ -5,15 +5,17 @@
 #include "holberton.h"
 #include <stddef.h>
 /**
-* _puts - prints string variable to stdout
-* @str: variable list of arguments
-* Return: void
+* op_string - prints string variable to stdout
+* @args: variable list of arguments
+* Return: 0
 */
 int op_string(va_list args)
 {
 	int i;
 	char *s = va_arg(args, char *);
 
+	if (!s)
+		return (-1);
 	for (i = 0; s[i] != '\0'; i++)
 		_putchar(s[i]);
 	return (0);
