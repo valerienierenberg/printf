ac6c1ac4a15d7f1aeade9fc9142258c04542dce7
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-26 14:55:24 +0000

    Remove extra functions from functions.c

--- functions.c
+++ functions.c
@@ -25,18 +25,4 @@ void _puts(char *str)
 * @b: second integer
 * Return: result of division of a by b
 */
-int op_div(int a, int b)
-{
-return (a / b);
-}
 
-/**
-* op_mod- returns remainder of divivsion of two integers
-* @a: first integer
-* @b: second integer
-* Return: remainder of division of a by b
-*/
-int op_mod(int a, int b)
-{
-return (a % b);
-}
