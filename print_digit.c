bac76cdc9b228d8f70d22da24b6b5e220d581750
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-28 15:28:31 +0000

    Add comments to print_digit.c

--- print_digit.c
+++ print_digit.c
@@ -13,27 +13,31 @@ int op_int(va_list args)
 	char a;
 	int b;
 	char c;
-	int n = va_arg(args, char *);
+	int n = va_arg(args, char *); /* set variable arguments to type int */
 
 	if (n < 0)
 	{
 		_putchar('-');
 		count++;
+/* change the number to char type (a) so that we can use putchar later */
 		a = ('0' - (n % 10));
-		n /= -10;
+		n /= -10; /* get the next digit over to the left */
 	}
+/* Here, the argument is zero or a positive number */
 	else
 	{
 		a = ((n % 10) + '0');
 		n /= 10;
 	}
 	b = 0;
+/* loops through the number getting one digit over to the left each time */
 	while (n > 0)
 	{
 		b = b * 10 + (n % 10);
 		n /= 10;
 	}
 
+/* this loop prints each digit backwards from left to right (_putchar (c))*/
 	while (b > 0)
 	{
 		c = ((b % 10) + '0');
@@ -41,8 +45,8 @@ int op_int(va_list args)
 		count++;
 		b /= 10;
 	}
+/* print right-most digit of argument */
 	_putchar(a);
 	count++;
-
 	return (count);
 }
