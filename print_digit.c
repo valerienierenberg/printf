e81fdcdf3c1c6c63d21659c2571f3608e41b75a7
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-28 15:36:34 +0000

    Add comments to print_digit.c

--- print_digit.c
+++ print_digit.c
@@ -13,36 +13,36 @@ int op_int(va_list args)
 	char a;
 	int b;
 	char c;
-	int n = va_arg(args, char *);
+	int n = va_arg(args, char *); /* set type of variable arguments to int */
 
-	if (n < 0)
+	if (n < 0) /* argument is negative */
 	{
 		_putchar('-');
-		count++;
-		a = ('0' - (n % 10));
-		n /= -10;
+		count++; /* add to count each time a character is printed */
+		a = ('0' - (n % 10)); /* assign last digit to a (char type) */
+		n /= -10; /* move over one digit (place) to the left */
 	}
-	else
+	else /* argument is zero or positive */
 	{
-		a = ((n % 10) + '0');
-		n /= 10;
+		a = ((n % 10) + '0'); /* assign last digit to a (char type) */
+		n /= 10; /*move over one digit (place) to the left */
 	}
 	b = 0;
-	while (n > 0)
+	while (n > 0) /* loop thru argument moving one place to left each time */
 	{
 		b = b * 10 + (n % 10);
 		n /= 10;
 	}
 
-	while (b > 0)
+	while (b > 0) /* loop to print each digit (c), from left to right */
 	{
 		c = ((b % 10) + '0');
 		_putchar(c);
 		count++;
 		b /= 10;
 	}
-	_putchar(a);
+	_putchar(a); /* print-right most digit */
 	count++;
 
-	return (count);
+	return (count); /* = chars printed, now added to numprinted in _printf */
 }
